376929870b3d09024e965c18f84ead416b0b3906
Nina Bobyleva <2847171@mail.ru>
2024-10-04 20:56:18 +0300

    Проект готов к началу работы

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jest-environment-jsdom",
+
+  moduleNameMapper: {
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^/public/img/(.*)$": "<rootDir>/public/img/$1",
+  },
+  transform: {
+    "^.+\\.(svg)$": "<rootDir>/node_modules/jest-transform-stub",
+    ".[jt]sx?$": "babel-jest",
+  },
+};
+
+export default config;
