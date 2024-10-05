fe73f6765aa7dc8593b625691b5f0f1e8faa7793
Nina Bobyleva <2847171@mail.ru>
2024-10-05 16:49:15 +0300

    Начальная верстка и стили готовы

---
+++ src/components/Search/search.styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  width: 1100px;
+  height: 50px;
+  outline: none;
+  padding: 20px;
+  border-radius: 30px;
+  ::placeholder {
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 3px;
+    color: #94a6be;
+  }
+`;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,19 @@
-import { Title } from "./homePage.styled.ts";
+import { Button } from "../../components/Button/Button.tsx";
+import { Search } from "../../components/Search/Search.tsx";
+import { UserList } from "../../components/UserList/UserList.tsx";
+import * as S from "./homePage.styled.ts";
 
 export function HomePage() {
-    return(
-        <>
-        <Title>Поиск пользователей Git Hub</Title>
-        </>
-    );
+  return (
+    <>
+      <S.Container>
+        <S.SearchBox>
+          <S.Title>Поиск пользователей на Git Hub</S.Title>
+          <Search />
+          <Button />
+          <UserList />
+        </S.SearchBox>
+      </S.Container>
+    </>
+  );
 }
