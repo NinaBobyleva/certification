c34eb36fa059ad193722d4a88b69c9de3748cc53
Nina Bobyleva <2847171@mail.ru>
2024-10-08 21:28:45 +0300

    correct

--- src/api/userInfo.ts
+++ src/api/userInfo.ts
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const token = import.meta.env.VITE_TOKEN;
 
+const config = {
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+};
+
 export async function getUsers(
   login: string,
   perPage: number,
@@ -18,22 +24,18 @@ export async function getUsers(
     sortResult = "&sort=repositories&order=desc";
   }
 
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-
   const response = await axios.get(
     `https://api.github.com/search/users?q=${login}+&page=${currentPage}${sortResult}&per_page=${perPage}`,
-    // config
+    config
   );
 
   return response.data;
 }
 
 export async function getUserInfo(login: string) {
-  const response = await axios.get(`https://api.github.com/users/${login}`);
+  const response = await axios.get(`https://api.github.com/users/${login}`,
+    config
+  );
 
   return response.data;
 }
