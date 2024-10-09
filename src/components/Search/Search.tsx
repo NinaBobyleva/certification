791f63fa834f2f2a0e1aab8f2287c31a51df725e
Nina Bobyleva <2847171@mail.ru>
2024-10-09 11:10:40 +0300

    Добавила debounce

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,17 +1,20 @@
 import * as S from "./search.styled";
+import { debounce } from "lodash";
 
 export function Search({
   setLogin,
 }: {
   setLogin: React.Dispatch<React.SetStateAction<string>>;
 }) {
+  const debounceSearch = debounce((e: React.ChangeEvent<HTMLInputElement>) => {
+    setLogin(e.target.value);
+  }, 1000);
+
   return (
     <S.Input
-      type="text"
+      type="search"
       placeholder="Введите логин пользователя"
-      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-        setLogin(e.target.value)
-      }
+      onChange={debounceSearch}
     />
   );
 }
